a97728b3bd07376ca5d54d42408ced4166ecd3a2
Yujilong <2069388340@qq.com>
2020-12-11 10:39:50 +0800

    change winner cash icon

--- Assets/Scripts/UI/Assist/BettingWinnerItem.cs
+++ Assets/Scripts/UI/Assist/BettingWinnerItem.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class BettingWinnerItem : MonoBehaviour
 {
     public Image head_iconImage;
+    public GameObject cash_iconGo;
     public Text nameText;
     public Text prize_cash_num_Text;
     public void Init(int head_id, string name,int cashNum)
     {
         head_iconImage.sprite = Sprites.GetSprite(SpriteAtlas_Name.HeadIcon, "head_" + head_id);
         nameText.text = name;
-        prize_cash_num_Text.text = "$" + cashNum.GetCashShowString();
+        bool isPackB = Save.data.isPackB;
+        prize_cash_num_Text.text = (isPackB ? "$" : "") + cashNum.GetCashShowString();
+        cash_iconGo.SetActive(isPackB);
     }
 }
